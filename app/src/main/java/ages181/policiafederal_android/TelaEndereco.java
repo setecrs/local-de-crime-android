ac1220af262a1d455f76700424ebcf6022ff11e5
Joao Soares <joao.etchichury@acad.pucrs.br>
2018-04-14 18:39:54 -0300

    Agora não só Spinners são salvos em cache, mas os EditTexts também. Algumas mudanças menores de código como: alteração de nome de variaveis e metodos foram feitas.

--- app/src/main/java/ages181/policiafederal_android/TelaEndereco.java
+++ app/src/main/java/ages181/policiafederal_android/TelaEndereco.java
@@ -4,11 +4,12 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class TelaEndereco extends AppCompatActivity {
 
@@ -29,52 +30,80 @@ public class TelaEndereco extends AppCompatActivity {
         editTextNumero = findViewById(R.id.editTextNumero);
         editTextComplemento = findViewById(R.id.editTextComplemento);
 
-        spinnerSelectedListener(spinnerLocal, "spinnerLocal");
-        spinnerSelectedListener(spinnerEstado, "spinnerEstado");
-        spinnerSelectedListener(spinnerCidade, "spinnerCidade");
+        criaFuncionalideSalvarSpinner(spinnerLocal, "spinnerLocal");
+        criaFuncionalideSalvarSpinner(spinnerEstado, "spinnerEstado");
+        criaFuncionalideSalvarSpinner(spinnerCidade, "spinnerCidade");
 
-        carregaInfromacoesSpinners("spinnerLocal", spinnerLocal);
-        carregaInfromacoesSpinners("spinnerEstado", spinnerEstado);
-        carregaInfromacoesSpinners("spinnerCidade", spinnerCidade);
+        criaFuncionalidadeSalvarTexto(editTextRua, "editTextRua");
+        criaFuncionalidadeSalvarTexto(editTextNumero, "editTextNumero");
+        criaFuncionalidadeSalvarTexto(editTextComplemento, "editTextComplemento");
+
+        carregaConteudoEditText( "editTextRua", editTextRua);
+        carregaConteudoEditText( "editTextNumero", editTextNumero);
+        carregaConteudoEditText( "editTextComplemento", editTextComplemento);
+
+        carregaInfromacaoSpinners("spinnerLocal", spinnerLocal);
+        carregaInfromacaoSpinners("spinnerEstado", spinnerEstado);
+        carregaInfromacaoSpinners("spinnerCidade", spinnerCidade);
     }
 
-    private void salvaPosicaoSpinner(String spinner, int posicao){
+    private void salvaPosicaoSpinner(String nomeSpinner, int posicao){
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putInt(spinner, posicao);
-        editor.commit();
+        editor.putInt(nomeSpinner, posicao);
+        editor.apply();
     }
 
-    private void carregaInfromacoesSpinners(String nomeSpinner, Spinner spinner){
+    private void salvaConteudoEditText(String nomeEditText, String texto){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(nomeEditText, texto);
+        editor.apply();
+    }
+
+    private void carregaInfromacaoSpinners(String nomeSpinner, Spinner spinner){
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         int posicaoSpinner = sp.getInt(nomeSpinner, 0);
         spinner.setSelection(posicaoSpinner);
     }
 
-    private void spinnerSelectedListener(Spinner spinner, final String nomeSpinner){
+    private void carregaConteudoEditText(String nomeEditText, EditText editText){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String texto = sp.getString(nomeEditText, null);
+        editText.setText(texto);
+    }
 
+    private void criaFuncionalideSalvarSpinner(Spinner spinner, final String nomeSpinner){
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int posicao, long id) {
-                String selectedItemText = (String) parent.getItemAtPosition(posicao);
                 if(posicao == 0){
                     // manda o que pro banco de dados... null?
-                    Toast.makeText(getApplicationContext(), "Selected(NULL): " + selectedItemText, Toast.LENGTH_SHORT)
-                          .show();
                     salvaPosicaoSpinner(nomeSpinner, posicao);
                 }else {
                     // salvar no BD
-                    //Toast.makeText(getApplicationContext(), "Selected: " + selectedItemText, Toast.LENGTH_SHORT)
-                    //      .show();
                     salvaPosicaoSpinner(nomeSpinner, posicao);
                 }
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            public void onNothingSelected(AdapterView<?> adapterView) {  }
+        });
+    }
 
+    private void criaFuncionalidadeSalvarTexto(final EditText editText, final String nomeEditText){
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {  }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {  }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                salvaConteudoEditText(nomeEditText, editable.toString());
             }
         });
-
     }
 }
