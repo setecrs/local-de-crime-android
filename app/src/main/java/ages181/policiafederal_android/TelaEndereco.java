6eefcbb8e7e3b05bce32ff2af8673717bb47beb4
17180512 <marc.herrmann@acad.pucrs.br>
2018-06-01 17:08:52 -0300

    Integração classe httpEndereco (João Soares e Marc)

--- app/src/main/java/ages181/policiafederal_android/TelaEndereco.java
+++ app/src/main/java/ages181/policiafederal_android/TelaEndereco.java
@@ -161,10 +161,14 @@ public class TelaEndereco extends Fragment{
             HttpEndereco t  = new HttpEndereco(local, spinnerEstado.getSelectedItem().toString(), autoCompleteTextViewCidade.getText().toString(),
                                         editTextRua.getText().toString(), editTextNumero.getText().toString(), editTextComplemento.getText().toString(),
                                         StaticProperties.getId());
+<<<<<<< 83834c7a946281bfb6f131758a880904ade0616e
 <<<<<<< 2c2a98881c0c45298b6cb9b57f6e272e8583f46f
             t.execute();
 =======
 >>>>>>> Classe estática, TOAD de usário ou senha inválidos. (João Soares, Marc Hermann)
+=======
+            t.execute();
+>>>>>>> Integração classe httpEndereco (João Soares e Marc)
         }catch (Exception e){
             e.printStackTrace();
         }
