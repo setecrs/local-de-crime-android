bbb20b5d0524333f7dd66203571e7d4ffe385b03
Joao Garcia <joao.honscha@acad.pucrs.br>
2018-06-09 12:12:56 -0300

    merge testemunha

--- app/src/main/java/ages181/policiafederal_android/TelaTestemunhas.java
+++ app/src/main/java/ages181/policiafederal_android/TelaTestemunhas.java
@@ -7,15 +7,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class TelaTestemunhas extends Fragment {
+
+    private EditText editTextNomeTestemunha, editTextFuncaoTestemunha, editTextDocTestemunha, editTextEntrevistaTestemunha;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View v = inflater.inflate(R.layout.testemunhas, container, false);
+
+        editTextNomeTestemunha = v.findViewById(R.id.etNome);
+        editTextFuncaoTestemunha = v.findViewById(R.id.etFuncao);
+        editTextDocTestemunha = v.findViewById(R.id.etDocumento);
+        editTextEntrevistaTestemunha = v.findViewById(R.id.etEntrevista);
+
+        carregaTestemunha();
+
+
         return v;
     }
+
+
+    public void carregaTestemunha(){
+
+        editTextNomeTestemunha.setText(CarregarOcorrencia.getTestNome());
+        editTextFuncaoTestemunha.setText(CarregarOcorrencia.getTestFuncao());
+        editTextDocTestemunha.setText(CarregarOcorrencia.getTestDoc());
+        editTextEntrevistaTestemunha.setText(CarregarOcorrencia.getTestEntrevista());
+    }
+
+
 
     public static TelaTestemunhas newInstance() {
         TelaTestemunhas f = new TelaTestemunhas();
